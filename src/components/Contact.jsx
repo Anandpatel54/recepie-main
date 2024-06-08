f37424b2b379ff9cff17fd002cfa327084421a8f
Anand patel <panand054@gmail.com>
2024-06-08 19:01:31 +0530

    first commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+const Contact = () => {
+  return (
+    <div className="w-[70%] m-auto mt-[10%] p-10 bg-green-100 rounded-lg">
+      <h1 className="text-7xl mt-5 font-extrablod text-green-600 mb-[5%]">
+        DO YOU WANT TO REACH US ?
+      </h1>
+      <button className="rounded-md  text-md bg-green-600 text-white py-2 px-2 hover:bg-green-700 duratoin-200">
+        REACH KRLO FRINDES &nbsp; &#8594;
+      </button>
+    </div>
+  );
+};
+
+export default Contact;
